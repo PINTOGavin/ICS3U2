9bdf90b44a3583eaddb9cfcc12df08a7955d122a
PINTOGavin <pintog001@tcdsb.ca>
2022-04-04 22:18:20 -0400

    renamed and updated changeThatMessage

--- Unit4/src/U4A4changeThatMessage.java
+++ Unit4/src/U4A4changeThatMessage.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
-public class changeThatMessage {
+public class U4A4changeThatMessage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//calls welcome
 		welcome();
+		
+		//asks for a phrase and stores in a string
 		System.out.print("Enter a phrase up to 6 words long: ");
 		Scanner scanner = new Scanner(System.in);
 		String message = scanner.nextLine();
 		
+		//checks if string is over 6 words long
 		if (countWords(message)>6) {
 			System.out.println("Not a valid message. Goodbye!");
+			//terminates program
 			return;
 		}
 		
+		//manipulates the phrase in different ways
 		System.out.println("\nHere are your manipulated messages: ");
 		System.out.println("UPPER: " + message.toUpperCase());
 		System.out.println("lower: " + message.toLowerCase());
@@ -20,8 +25,14 @@ public class changeThatMessage {
 		System.out.println("AlTeRnAtInG: " + altCase(message));
 		System.out.println("# Vowels: " + countVowels(message) + " vowels");
 		System.out.println("vOwEls In UppEr: " + vowelUpper(message));
-		
-	}
+		System.out.println("Reverse: " + reverseMessage(message));
+		if (message.indexOf(' ')==-1 && message.length()%2==0) {
+			System.out.println("Word Stack: \n");
+			wordStack(message);
+		}else {
+			System.out.println("Word Stack not available. Please enter one word that is an even length next time.");
+		}
+	}//end main
 	
 	public static void welcome() {
 		System.out.println("              ,..........   ..........,");
@@ -34,7 +45,7 @@ public class changeThatMessage {
 		System.out.println("      '''''''''''''''''';''';''''''''''''''''''");
 		System.out.println("                         '''");
 		System.out.println("\n       Welcome to the Message Manipulator!");
-	}
+	}//end welcome
 	
 	public static int countWords(String message) {
 		int wordCnt = 1;
@@ -44,7 +55,7 @@ public class changeThatMessage {
 			}
 		}
 		return wordCnt;
-	}
+	}//end countWords
 	
 	public static String altCase(String message) {
 		String newMessage = "";
@@ -58,34 +69,54 @@ public class changeThatMessage {
 			newMessage = newMessage + letter;
 		}
 		return newMessage;
-	}
+	}//end altCase
 	
 	public static int countVowels(String message) {
 		int vowelCnt = 0;
 		String messageLower = message.toLowerCase();
 		for (int i = 0; i<message.length(); i++) {
-			if (messageLower.charAt(i)=='a' || messageLower.charAt(i)=='e' || messageLower.charAt(i)=='i' || messageLower.charAt(i)=='o' || messageLower.charAt(i)=='u') {
+			char letter = messageLower.charAt(i);
+			if (letter=='a' || letter=='e' || letter=='i' || letter=='o' || letter=='u') {
 				vowelCnt++;
 			}
 		}
 		return vowelCnt;
-	}
+	}//countVowels
 	
 	public static String vowelUpper(String message) {
 		String messageLower = message.toLowerCase();
 		String newMessage = "";
 		for (int i = 0; i<message.length(); i++) {
 			char letter = messageLower.charAt(i);
-			if (messageLower.charAt(i)=='a' || messageLower.charAt(i)=='e' || messageLower.charAt(i)=='i' || messageLower.charAt(i)=='o' || messageLower.charAt(i)=='u') {
+			if (letter=='a' || letter=='e' || letter=='i' || letter=='o' || letter=='u') {
 				letter = Character.toUpperCase(letter);
 			}
 			newMessage = newMessage + letter;
 		}
 		return newMessage;
-	}
+	}//end vowelUpper
 	
 	public static String reverseMessage(String message) {
-		return "";
-	}
-
-}
+		String newMessage = "";
+		int cnt = message.length()-1;
+		while (cnt>=0) {
+			newMessage = newMessage + message.charAt(cnt);
+			cnt--;
+		}
+		return newMessage;
+	}//end reverseMessage
+	
+	public static void wordStack(String message) {
+		int runs = message.length()/2;
+		int spaces = runs-1;
+		for (int i = 1; i<=runs; i++) {
+			String newMessage = "";
+			for (int j = spaces; j>0; j--) {
+				newMessage = newMessage + " ";
+			}
+			newMessage = newMessage + message.substring(runs-i,runs+i);
+			System.out.println(newMessage);
+			spaces--;
+		}
+	}//end wordStack
+}//end class
